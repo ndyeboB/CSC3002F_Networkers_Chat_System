2067cf77020603e5c6cbb1c1671bae478f57abee
Blessings Nyameni <nymndy001@myuct.ac.za>
2026-03-04 01:59:52 +0200

    Implemented a interface and default group chat

--- CHAT_SERVER.py
+++ CHAT_SERVER.py
@@ -2,14 +2,14 @@
 import socket
 import threading
 
-HOST = "0.0.0.0"
+HOST = "127.0.0.1"
 PORT = 1234 # the range of use is 0 to 65535
 
 CLIENT_LIMIT = 5 # lets set a limit on the amount of people that can be a chat -- later we can build more capacity in our system
 
 online_clients = [] # list of all clients that are curently online and connected to the server
 
-
+groups = {"default": []} # default group just to show group chat demonstartion
 
 
 
@@ -18,14 +18,27 @@ def work_with_client(client):
 
     while True:
         username = client.recv(2048).decode('utf-8') # wait for the username that yo may receive from the client
-        if username != '':
+        
+        if username == "":
+            print("The username given by the client is empty!")
+
+
+        username_used = False
+        for user in online_clients: # loop in the online_list to check if username already exists
+            if user[0] == username:
+                username_used = True
+                break
+        if username_used: # if the username has been found
+            client.send("ERROR:Username has been already taken".encode())
+        else:
             online_clients.append((username, client)) # we add the client to list
-            entry_message = "Server: "+f"{username} has entered the chat system!"
-            lets_send_message_to_everyone(entry_message) 
+            
+            client.send("ACK:Login successful!\n".encode()) # we tell the client login is successful!
+            entry_message = "SERVER: "+f"{username} has entered the chat system!\n"
+            lets_send_message_to_everyone(entry_message, exclude_username=username) 
             # when a new user is added to the chat, every one else is notified
             break
-        else:
-            print("The username given by the client is empty!")
+        
     # this CREATES a NEW THREAD
     # that thread runs server_listening
     # SO EACH CLIENT HAS ITS OWN LOOP!!!!!
@@ -39,12 +52,40 @@ def server_listening(client, username): # responsible of collecting the message
     while True:
         message = client.recv(2048).decode('utf-8') # listens for the message that the client wants to send
 
-        if message !='':
-            main_message = username + ':' + message
-            lets_send_message_to_everyone(main_message)
+        if message =='':
+            #main_message = username + ':' + message
+            #lets_send_message_to_everyone(main_message)
+            continue
 
+        # Choice 1: Connect to friend (peer)
+        if message == "1":
+            client.send("SERVER:Zamashengu is still working on this feature!".encode())
+        # Choice 2: Join default group
+        elif message == "2":
+            if username not in groups["default"]: # check if you are not in the group already
+
+                groups["default"].append(username)
+                client.send("SERVER:You have joined the default group.".encode())
+            else:
+                client.send("SERVER:You are already in the group.".encode())
+
+        elif message =="3":
+            client.send("SERVER:Exiting chat...".encode())
+            online_clients.remove((username, client)) # remove the user off the in-memory
+
+            if username in groups["default"]:
+                groups["default"].remove(username)
+            client.close()
+            break
+        
+        # user has already pressed a option, --> default group
         else:
-            print(f"The message sent from client: {username} is empty. Please try again.")
+            if username in groups["default"]:
+                group_message = f"{username}: {message}"
+                send_to_default_group(group_message)
+            else:
+                client.send("SERVER:Join the default group first (option 2)".encode())
+            
     
     
 # this is to send a message to a single client
@@ -53,10 +94,20 @@ def lets_send_message_to_client(client, message):
     client.sendall(message.encode()) # client = receiving client
 
 # needs to be further implemented so we first initiate a seperate group chat to send to everyone
-def lets_send_message_to_everyone(message): 
+def lets_send_message_to_everyone(message, exclude_username=None): 
+        for username, client_socket in online_clients:
+            if username != exclude_username:   # this ensures that we broadcast to everyone except the user who sent it
+                client_socket.sendall(message.encode())
 
+        
+
+def send_to_default_group(message):
     for user in online_clients:
-        lets_send_message_to_client(user[1], message) # user[1] takes the client object because when we appended to the list we had username as the 1st element amd client as the 2md element
+        username = user[0]
+        client_socket = user[1]
+
+        if username in groups["default"]:
+            client_socket.sendall(message.encode()) # works the same way as the function lets_send_messages_to_client
 
 # main function
 def main():
--- client.py
+++ client.py
@@ -3,21 +3,28 @@
 import socket
 import threading
 
-HOST = '0.0.0.0 '   #Server IP address (IPv4)
+HOST = '127.0.0.1'   #Server IP address (IPv4)
 PORT = 1234              #Port number the server is listening on
+
 
 def listening_For_Messages(client):
 # This function continously listens for incoming messages from the server. It runs on a separate thread so that the client can send and receive messages simultaneously.
      while True:
-          message = client.recv(2048).decode('utf-8')
-          if message != '':
-               userID = message.split(":")[0]
-               messageContent = message.split(":")[1]
+          try: 
+               message = client.recv(2048).decode('utf-8')
+               if message != '':
+                    userID = message.split(":")[0]
+                    messageContent = message.split(":")[1]
 
-               print(f"[{userID}]: {messageContent}")
-               
-          else:
-               print("Message is empty!")
+                    print(f"[{userID}]: {messageContent}")
+                    
+                    
+               else:
+                    print("Message is empty!")
+          except:
+               print("Disconnected from the server")
+               break
+
 
 def send_message(client):
     # This function allows the user to type messages and send them to the server.
@@ -31,17 +38,62 @@ def send_message(client):
 
 def server_communication(client):
 #Handles initial communication with the server, including sending the userID and starting the listening thread.
-     userID = input("Login..\nEnter userID: ")
-     if userID != '':
-          client.sendall(userID.encode())
-     else:
-          print("Invalid: UserID cannot be empty")
-          exit(0)
-     #start the listening thread
+     while True:
+          userID = input("\nLogin..\nEnter userID: ")
+          if userID != '':
+               client.sendall(userID.encode())
+               receiving_msg = client.recv(2048).decode() # the client receives the server responce about login success/error
+
+               print(receiving_msg) # we take the server response
+               if receiving_msg.startswith("ACK"): # if the server ACKnowledged username succes
+                    break
+          else:
+               print("Invalid: UserID cannot be empty")
+               
+          #start the listening thread after login success
      threading.Thread(target=listening_For_Messages, args=(client, )).start()
-  
-     # main thread that handles sending messages
+
+     interface_menu(client) # after login success, show the menu
+     
+          # main thread that handles sending messages
      send_message(client)
+
+
+def interface_menu(client):
+     while True:
+          print("\nWELCOME TO THE NETWORKERS CHAT SYSTEM!!")
+          print("1. Connect to Peer")
+          print("2. Join Default Group")
+          print("3. Send messages to Default group")
+          print("4. Exit")
+
+          choice = input("Choose your action: \n")
+
+          if choice in ["1", "2", "4"]:
+               client.sendall(choice.encode())
+
+               if choice == "4":
+                    break
+
+          elif choice == "3":
+               send_group_message(client) # we need to make a function that allows messaging
+          
+          else:
+               print("Invalid choice!")
+
+def send_group_message(client):
+     print("\nWELCOME TO OUR DEFAULT GROUP CHAT!!")
+     print("Type 'return' to go back to the menu")
+
+     while True:
+          message = input() # you write
+
+          if message.lower() == 'back': # exit
+               break
+          if message != "":
+               client.sendall(message.encode())
+
+
 
 #Main function
 def main():
