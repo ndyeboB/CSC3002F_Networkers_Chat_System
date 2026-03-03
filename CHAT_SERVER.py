0bc8b6dfe428203137d6c62af2842b2211089959
Blessings Nyameni <nymndy001@myuct.ac.za>
2026-03-03 19:41:15 +0200

    Added comments

--- CHAT_SERVER.py
+++ CHAT_SERVER.py
@@ -14,13 +14,13 @@ online_clients = [] # list of all clients that are curently online and connected
 
 
 def work_with_client(client):
-    # server will listen for client message that will contain the username 
+    # initial connection with the client
 
     while True:
         username = client.recv(2048).decode('utf-8') # wait for the username that yo may receive from the client
         if username != '':
             online_clients.append((username, client)) # we add the client to list
-            entry_message = "Server:"+f"{username} has entered the chat system!"
+            entry_message = "Server: "+f"{username} has entered the chat system!"
             lets_send_message_to_everyone(entry_message) 
             # when a new user is added to the chat, every one else is notified
             break
@@ -47,10 +47,10 @@ def server_listening(client, username): # responsible of collecting the message
 # this is to send a message to a single client
 # needs to be further implemented so the client can choose who to send a message to with peer to peer
 def lets_send_message_to_client(client, message):
-    client.sendall(message.encode)
+    client.sendall(message.encode) # client = receiving client
 
 # needs to be further implemented so we first initiate a seperate group chat to send to everyone
-def lets_send_message_to_everyone(message):
+def lets_send_message_to_everyone(message): 
 
     for user in online_clients:
         lets_send_message_to_client(user[1], message) # user[1] takes the client object because when we appended to the list we had username as the 1st element amd client as the 2md element
